78e5a3056bcfa83db4259db847c4e72e7bb9b19d
teampofy <58967438+teampofy@users.noreply.github.com>
2021-11-17 09:20:23 -0500

    Add project files.

---
+++ Elephant.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Elephant
+{
+    public string Name;
+    public int EarSize;
+
+    public static string userResponse;
+
+    //to redirect the order of the list
+    public bool reverseIndicator = false;
+
+    //create a list object to arrange the sequence if user presses 3
+    public static List<string> elephantList = new List<string>();
+
+    /// <summary>
+    /// Issue is that i have is that i want to you a list to store elephant infomation
+    /// Based on the reverse indicator flag, I will return the information
+    /// But im stuck trying to use this concept and so my method can take care of everything
+    /// then i would like to create a question method which tells customers their options and then assigns the value to 
+    /// my whoami method to run on its own, (but not sure how to create a method which takes user input and then store to a variable
+    /// </summary>
+    /// <param name="whoDoYouWantToCall"></param>
+    public void WhoAmI(int whoDoYouWantToCall)
+    {   
+        //check the validtiy of the input
+        if (whoDoYouWantToCall == 1 || whoDoYouWantToCall == 2)
+        {
+            //if option 1 is selected without a reverse request from option 3
+            if (whoDoYouWantToCall == 1 && reverseIndicator == false)
+            {
+                Console.WriteLine("Calling " + elephantList[0].Name + " whose ear size is " + elephantList[0].Earsize + " .");
+            }
+
+            //if option 1 is selected with a reverse request from option 3
+            else if (whoDoYouWantToCall == 1 && reverseIndicator == true)
+            {
+                Console.WriteLine("Calling " + elephantList[1].Name + " whose ear size is " + elephantList[1].Earsize + " .");
+            }
+
+            //if option 2 is selected without a reverse request from option 3
+            else if (whoDoYouWantToCall == 2 && reverseIndicator == false)
+            {
+                Console.WriteLine("Calling " + elephantList[1].Name + " whose ear size is " + elephantList[1].Earsize + " .");
+            }
+
+            //if option 2 is selected with a reverse request from option 3
+            else 
+            {
+                Console.WriteLine("Calling " + elephantList[0].Name + " whose ear size is " + elephantList[0].Earsize + " .");
+            }
+        }
+
+        else if (whoDoYouWantToCall == 3)
+        {
+            if (reverseIndicator == false)
+            {
+                reverseIndicator = true;
+                Console.WriteLine("The two references have been exchanged! ");
+            }
+
+            else
+            {
+                reverseIndicator = false;
+                Console.WriteLine("The two references have been exchanged! ");
+            }
+        }
+
+        else if (whoDoYouWantToCall == 4)
+        {
+            Console.WriteLine("Thank you for playing until, take care and stay safe! ");
+            System.Environment.Exit(10);
+        }
+
+        else
+        {
+            Console.WriteLine("That was not a valid option, good bye, take care, and stay safe! ");
+            System.Environment.Exit(10);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Chapter4ElephantExercisePage194
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create the elephants
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+            Console.WriteLine("Welcome to the elephant game.\n " +
+            "Please enter a value of 1 to call the elephant 1.\n" +
+            "Please enter a value of 2 to call the elephant 2.\n" +
+            "Please enter a value of 3 to resort the order of the elephants that you want to call.\n" +
+            "Or please enter a value of 4 to end this game.");
+            
+            string userResponse = Console.ReadLine();
+
+            WhoAmI(userResponse);
+
+
+        }
+    }
+}
